cd90f58b17d3c26aa1b54287767f69bfd8321cef
Roger Poore <rpoore@gmail.com>
2018-06-26 17:35:13 +0000

    Update Home.js

--- AppRead/Home/Home.js
+++ AppRead/Home/Home.js
@@ -1,91 +1,73 @@
-﻿/// <reference path="../App.js" />
-// global app
-
-(function () {
-    'use strict';
-
-    // The Office initialize function must be run each time a new page is loaded
-    Office.initialize = function (reason) {
-        $(document).ready(function () {
-            app.initialize();
-			
-			// start this baby....
-			sendRequest();
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+ 
+(() => {
+  // The initialize function must be run each time a new page is loaded
+  Office.initialize = (reason) => {
+    $(document).ready(() => {    
+      var item = Office.context.mailbox.item;
+      getBody();
+    });
+  };
+ 
+  function getBody() {
+    var _item = Office.context.mailbox.item;
+    body = _item.body;
+   
+    // Get the body asynchronous as text
+    body.getAsync(Office.CoercionType.Text, function (asyncResult) {
+        if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
+            // TODO: Handle error
+        }
+        else {
+            // Show data
+            var bodytext = asyncResult.value.trim();
+ 
+            checkBody(bodytext);
+        }
+    });
+   }
+ 
+   function checkBody(bodytext) {
+        $.ajax({
+            type: 'POST',
+            url: 'https://webservices.clayton.edu/officeapps/SafetyCheck/AppRead/Home/Handler.ashx',
+            data: {
+                task: 'checkBody',
+                msg: encodeURIComponent(bodytext),
+            },
+            success: function (returnvalue) {              
+                var isMal = returnvalue;
+ 
+                if (isMal == 'Y') {
+                    displayBad();
+                }
+                else {
+                    $('#run').show();
+                    displayGood();
+                }
+            }
         });
-    };
-
-    function sendRequest() {
-        // Create a local variable that contains the mailbox.
-        var mailbox = Office.context.mailbox;
-
-        mailbox.makeEwsRequestAsync(getBodyRequest(mailbox.item.itemId), callback);
     }
-	
-	function displayGood() {
-		var msg = "This email has not been reported as malicious, but it should be reported if the content looks suspicious. " +
-				  "What to look for: <ul><li>Poor grammar and spelling</li>" +
-			      "<li>Requests for financial information</li>" +
+ 
+  function displayGood() {
+        var msg = "This email has not been reported as malicious but that doesn't mean it's not. " +
+                  "What to look for: <br /><br /><ul><li>Poor grammar and spelling</li>" +
+                  "<li>Requests for financial information</li>" +
                   "<li>Requests for personal information such as a social security number or password</li>" +
-				  "<li>Links to sites that ask for the above information</li></ul>";
-				  
-		$('#statusImage').html('<img src="../../Images/tick.png" width="20" />');
-		$('#statusText').html(msg);		
-	}
-
-	function displayBad() {
-		$('#statusImage').html('<img src="../../Images/stop-ico-3a53fc3.png" />');
-		$('#statusText').text(" This e-mail contains known malicious content. You should disregard it!");		
-	}
-	
-	function getBodyRequest(id) {
-	   // Return a GetItem operation request for the body of the specified item. 
-	   var result = 
-		'<?xml version="1.0" encoding="utf-8"?>' +
-		'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
-		'               xmlns:xsd="http://www.w3.org/2001/XMLSchema"' +
-		'               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"' +
-		'               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">' +
-		'  <soap:Header>' +
-		'    <RequestServerVersion Version="Exchange2013" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" soap:mustUnderstand="0" />' +
-		'  </soap:Header>' +
-		'  <soap:Body>' +
-		'    <GetItem xmlns="http://schemas.microsoft.com/exchange/services/2006/messages">' +
-		'      <ItemShape>' +
-		'        <t:BaseShape>IdOnly</t:BaseShape>' +
-		'        <t:AdditionalProperties>' +
-		'            <t:FieldURI FieldURI="item:Body"/>' +
-		'        </t:AdditionalProperties>' +
-		'      </ItemShape>' +
-		'      <ItemIds><t:ItemId Id="' + id + '"/></ItemIds>' +
-		'    </GetItem>' +
-		'  </soap:Body>' +
-		'</soap:Envelope>';
-	
-	   return result;
-	}	
-	
-	function callback(asyncResult) {
-	   var result = asyncResult.value;
-	   var context = asyncResult.context;
-
-	   $.ajax({
-	       type: 'POST',
-	       url: '/officeapps/safetycheck/AppRead/Home/handler.ashx',
-	       data: {
-	           msg: encodeURIComponent(result),
-	       },
-	       success: function (returnvalue) {	           
-	           var isMal = returnvalue;
-
-	           if (isMal == 'Y') {
-	               displayBad();
-	           }
-	           else {
-	               displayGood();
-	           }
-	       }
-	   });
-	}
-
-
+                  "<li>Links to sites that ask for the above information</li></ul>";
+                   
+        $('#statusImage').html('<img src="assets/tick.png" width="50" />');
+        $('#statusText').html(msg);     
+    }
+ 
+  function displayBad() {
+         
+        $('#statusImage').html('<img src="assets/stop-ico.png" width="50"/>');
+        $('#statusText').text(" This e-mail contains known malicious content. You should disregard it!");       
+  }
+   
+ 
 })();
