650764820c2ea9bfcd7596a83449ece0da1e8108
Roger Poore <rpoore@gmail.com>
2018-06-26 13:11:44 -0400

    Initial

---
+++ AppRead/Home/Handler.ashx
@@ -0,0 +1,91 @@
+﻿<%@ WebHandler Language="C#" Class="Handler" %>
+
+using System;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+public class Handler : IHttpHandler 
+{
+    private string _connectionString = "Persist Security Info=False;Database=<db>;server=<server>;Port=3306;User Id=<username>;pwd=<passwd>";
+    
+    public void ProcessRequest (HttpContext context) 
+    {
+        context.Response.ContentType = "text/plain";        
+        string msg = HttpUtility.UrlDecode(context.Request.Form["msg"]);
+                
+        if (isPhish(msg))
+            context.Response.Write("Y");
+        else
+            context.Response.Write("N");        
+    }
+    
+    public bool isPhish(string msg)
+    {
+        bool returnVal = false;
+
+        MySqlConnection con = new MySqlConnection(_connectionString);
+        con.Open();
+
+        string sql = "select url from safety_check";
+
+        MySqlCommand cmd = new MySqlCommand(sql, con);
+        MySqlDataReader dr = cmd.ExecuteReader();
+
+        if (dr.HasRows)
+        {
+            while (dr.Read())
+            {
+                if (msg.Contains(dr["url"].ToString()))
+                {
+                    returnVal = true;
+                    break;
+                }
+            }
+        }
+
+        con.Close();        
+        
+        return returnVal;
+    }
+    
+    public string GetURLs()
+    {
+        string returnVal = null;
+        MySqlConnection con = new MySqlConnection(_connectionString);
+        con.Open();
+        
+        string sql = "select url from safety_check";
+        
+        MySqlCommand cmd = new MySqlCommand(sql, con);
+        MySqlDataReader dr = cmd.ExecuteReader();
+
+        if (dr.HasRows)
+        {
+            returnVal = "";
+        }        
+
+        con.Close();
+        return returnVal;   
+    }
+    
+    public void AddURL(string url,string addedby)
+    {
+        MySqlConnection con = new MySqlConnection(_connectionString);
+        con.Open();
+
+        string sql = "INSERT INTO safety_check (url,added_by) VALUES ('" + url + "','" + addedby + "')";
+        MySqlCommand cmd = new MySqlCommand(sql, con);
+        cmd.ExecuteNonQuery();        
+
+        con.Close();        
+    }
+ 
+    public bool IsReusable 
+    {
+        get 
+        {
+            return false;
+        }
+    }
+
+}
---
+++ AppRead/Home/Home.js
@@ -0,0 +1,91 @@
+﻿/// <reference path="../App.js" />
+// global app
+
+(function () {
+    'use strict';
+
+    // The Office initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+			
+			// start this baby....
+			sendRequest();
+        });
+    };
+
+    function sendRequest() {
+        // Create a local variable that contains the mailbox.
+        var mailbox = Office.context.mailbox;
+
+        mailbox.makeEwsRequestAsync(getBodyRequest(mailbox.item.itemId), callback);
+    }
+	
+	function displayGood() {
+		var msg = "This email has not been reported as malicious, but it should be reported if the content looks suspicious. " +
+				  "What to look for: <ul><li>Poor grammar and spelling</li>" +
+			      "<li>Requests for financial information</li>" +
+                  "<li>Requests for personal information such as a social security number or password</li>" +
+				  "<li>Links to sites that ask for the above information</li></ul>";
+				  
+		$('#statusImage').html('<img src="../../Images/tick.png" width="20" />');
+		$('#statusText').html(msg);		
+	}
+
+	function displayBad() {
+		$('#statusImage').html('<img src="../../Images/stop-ico-3a53fc3.png" />');
+		$('#statusText').text(" This e-mail contains known malicious content. You should disregard it!");		
+	}
+	
+	function getBodyRequest(id) {
+	   // Return a GetItem operation request for the body of the specified item. 
+	   var result = 
+		'<?xml version="1.0" encoding="utf-8"?>' +
+		'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
+		'               xmlns:xsd="http://www.w3.org/2001/XMLSchema"' +
+		'               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"' +
+		'               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">' +
+		'  <soap:Header>' +
+		'    <RequestServerVersion Version="Exchange2013" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" soap:mustUnderstand="0" />' +
+		'  </soap:Header>' +
+		'  <soap:Body>' +
+		'    <GetItem xmlns="http://schemas.microsoft.com/exchange/services/2006/messages">' +
+		'      <ItemShape>' +
+		'        <t:BaseShape>IdOnly</t:BaseShape>' +
+		'        <t:AdditionalProperties>' +
+		'            <t:FieldURI FieldURI="item:Body"/>' +
+		'        </t:AdditionalProperties>' +
+		'      </ItemShape>' +
+		'      <ItemIds><t:ItemId Id="' + id + '"/></ItemIds>' +
+		'    </GetItem>' +
+		'  </soap:Body>' +
+		'</soap:Envelope>';
+	
+	   return result;
+	}	
+	
+	function callback(asyncResult) {
+	   var result = asyncResult.value;
+	   var context = asyncResult.context;
+
+	   $.ajax({
+	       type: 'POST',
+	       url: '/officeapps/safetycheck/AppRead/Home/handler.ashx',
+	       data: {
+	           msg: encodeURIComponent(result),
+	       },
+	       success: function (returnvalue) {	           
+	           var isMal = returnvalue;
+
+	           if (isMal == 'Y') {
+	               displayBad();
+	           }
+	           else {
+	               displayGood();
+	           }
+	       }
+	   });
+	}
+
+
+})();
